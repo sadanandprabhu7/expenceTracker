6571419e4c5ef0952c2e307eaa10c67fa95e4b3d
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2023-01-13 03:52:40 +0530

    connection done with monodb

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
 // step 1
+const mongoose = require("mongoose");
 const express = require("express");
 const Razorpay = require("razorpay");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const fs = require("fs");
 
-const https = require("https");
-// const privateKey = fs.readFileSync("server.key");
-// const certificate = fs.readFileSync("server.cert");
-// step 2
 const app = express();
 
-const Expence = require("./model/expenceMain");
-const Urls = require("./model/url");
-const routesAdmin = require("./routes/user");
-const sequelize = require("./util/database");
+// const Expence = require("./model/expenceMain");
+// const Urls = require("./model/url");
+// const routesAdmin = require("./routes/user");
+// const sequelize = require("./util/database");
 
-const Forgotpassword = require("./model/forgotpassword");
-const resetPasswordRoutes = require("./routes/resetpassword");
+// const Forgotpassword = require("./model/forgotpassword");
+// const resetPasswordRoutes = require("./routes/resetpassword");
 
-const leaderShipRoutes = require("./routes/leadership");
+// const leaderShipRoutes = require("./routes/leadership");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const userRoutes = require("./routes/user");
-const purchaseRoutes = require("./routes/purchase");
-const Order = require("./model/orders");
-const bodyParser = require("body-parser");
-const User = require("./model/user_model");
-const path = require("path");
+// const userRoutes = require("./routes/user");
+// const purchaseRoutes = require("./routes/purchase");
+// const Order = require("./model/orders");
+// const bodyParser = require("body-parser");
+// const User = require("./model/user_model");
+// const path = require("path");
 
 // step 3
 const cors = require("cors");
@@ -43,42 +37,25 @@ const cors = require("cors");
 app.use(cors());
 //app.use(helmet());
 //app.use(morgan("combined", { stream: accessLogStream }));
-app.use(bodyParser.json({ extended: false }));
+// app.use(bodyParser.json({ extended: false }));
 
-app.use("/user", userRoutes);
+// app.use("/user", userRoutes);
 
-app.use("/purchase", purchaseRoutes);
+// app.use("/purchase", purchaseRoutes);
 
-// step 5
+// // step 5
 
-app.use("/password", resetPasswordRoutes);
+// app.use("/password", resetPasswordRoutes);
 
-app.use(routesAdmin);
-app.use(leaderShipRoutes);
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, `public/${req.url}`));
-});
-
-User.hasMany(Expence);
-Expence.belongsTo(User);
-///adddedd/// new file
-User.hasMany(Order);
-Order.belongsTo(User);
-
-User.hasMany(Forgotpassword);
-Forgotpassword.belongsTo(User);
-
-User.hasMany(Urls);
-sequelize
-  //.sync({ force: true })
-  .sync()
+// app.use(routesAdmin);
+// app.use(leaderShipRoutes);
+// app.use((req, res) => {
+//   res.sendFile(path.join(__dirname, `public/${req.url}`));
+// });
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nufu3hw.mongodb.net/?retryWrites=true&w=majority`
+  )
   .then((result) => {
-    //console.log(result);
-    // https
-    //   .createServer({ key: privateKey, cert: certificate }, app)
-    //   .listen(process.env.PORT);
     app.listen(process.env.PORT);
-  })
-  .catch((err) => {
-    console.log(err);
   });
